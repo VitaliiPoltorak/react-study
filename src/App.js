8056fd5056144cb09ba43bfb0341833bded5366b
ArtMyWeb <vitaliypoltorak91@gmail.com>
2021-10-26 19:16:22 +0300

    lesson 7

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import './App.css';
 function App() {
     return (
         <>
-            <div>first</div>
-            <div className="App">
-                <ul>
-                    <li>css</li>
-                    <li>html</li>
-                    <li>js</li>
-                    <li>react</li>
-                </ul>
-            </div>
+            <Header />
+            <Technologies />
+
         </>
     );
+}
+
+function Header() {
+    return (
+        <div>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a href="#">home</a>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a href="#">message</a>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a href="#">news</a>
+        </div>
+    )
+}
+
+function Technologies () {
+    return (
+        <div className="">
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
